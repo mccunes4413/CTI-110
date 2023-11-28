6362e0b86add20e6e29a65dee775325ff1ec69e0
mccunes4413 <146121965+mccunes4413@users.noreply.github.com>
2023-11-28 14:16:08 -0500

    second upload complete

--- P5HW_McCuneSamuel.py
+++ P5HW_McCuneSamuel.py
@@ -11,48 +11,55 @@ def show_menu():
     print("-------------------")
     print("1. Adding Random Numbers")
     print("2. Subtracting Random Numbers")
-    print("Exit Program")
+    print("3. Exit Program")
 
 def adding():
-    my_sum = random.randint(0,10) + random.randint(0,10)
-    print(my_sum)
+    random_1 = random.randint(0,10)
+    random_2 = random.randint(0,10)
+    my_sum = random_1 + random_2
+    print(f"      {random_1}")
+    print("+")
+    print(f"      {random_2}")
     return my_sum
 
 def subtracting():
-    my_diff = random.randint(0,10) - random.randint(0,10)
-    print(my_diff)
+    random_1 = random.randint(0,10)
+    random_2 = random.randint(0,10)
+    my_diff = random_1 - random_2
+    print(f"      {random_1}")
+    print("-")
+    print(f"      {random_2}")
     return my_diff
 
 def guessing(guess, value):
-    num_guess = 1
+    num_guess = 0
     while guess != value:
         num_guess += 1
+
         if guess > value:
                print("Your guess is too high")
                guess = int(input("Enter your guess: "))
         else:
                 print("Your guess is too low")
                 guess = int(input("Enter your guess: "))
-        print("Congrats, your guess was correct")
+    print("Congrats, your guess was correct")
+    print(f"It took you {num_guess} guesses")
 
 
 #Main function that runs the program
 def main():
-    
     while True:
-        num_guesses += 1
         show_menu()
         user_choice = int(input("Please choose one of the menu options: "))
         if user_choice == 1:
-           value = adding()
-           guess = int(input("Enter your guess: "))
-           guessing(guess, value)
-
+            value = adding()
+            guess = int(input("Enter your guess: "))
+            guessing(guess, value)
+            
         if user_choice == 2:
             value = subtracting()
             guess = int(input("Enter your guess: "))
             guessing(guess, value)
-
              
         if user_choice == 3:
             print("The Program has Ended")
